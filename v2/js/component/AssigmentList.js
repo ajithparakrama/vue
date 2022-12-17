5d32f2eef3d1f5eac2cef6ac658eafdf0d11d081
Ajith Perera <ajithpararkama@gmail.com>
2022-12-17 10:01:29 +0530

    ep 12

--- v2/js/component/AssigmentList.js
+++ v2/js/component/AssigmentList.js
@@ -6,9 +6,9 @@ export default {
     <section v-show="assigments.length" >
     <h2 class="font-bold mb-2">{{ title }}  <span> ({{ assigments.length }})</span> </h2>
     <assigment-tags 
+    v-model:currentTag="currentTag"
     :initial-tags="assigments.map(a=>a.tag)" 
     :current-tag="currentTag"
-    @change="currentTag=$event" 
     />
     <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
         <assigment  
