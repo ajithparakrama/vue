b916be9641eb4c80101000132d8c2239a07442a6
ajithparakrama <ajithpararkama@gmail.com>
2022-12-17 05:52:59 +0530

    ep 11

--- v2/js/component/AssigmentList.js
+++ v2/js/component/AssigmentList.js
@@ -1,16 +1,15 @@
 import Assigment from "./Assigment.js"
+import AssigmentTags from "./AssigmentTags.js";
 export default {
-    components:{Assigment},
+    components:{Assigment, AssigmentTags},
     template:`
     <section v-show="assigments.length" >
     <h2 class="font-bold mb-2">{{ title }}  <span> ({{ assigments.length }})</span> </h2>
-    <div class="flex gap-2">
-    <button 
-    v-for="tag in tags" 
-    @click="currentTag = tag" 
-    class="border rounded px-1 py-px text-xs" 
-    :class="{'border-blue-500 text-blue-500': tag==currentTag}">{{tag}}</button>
-    </div>
+    <assigment-tags 
+    :initial-tags="assigments.map(a=>a.tag)" 
+    :current-tag="currentTag"
+    @change="currentTag=$event" 
+    />
     <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
         <assigment  
         v-for="asign in filterdAssigments" 
@@ -39,9 +38,7 @@ export default {
             }
             return this.assigments.filter(a=>a.tag == this.currentTag);
         },
-        tags(){
-            return ['all', ...new Set(this.assigments.map(a => a.tag))];
-        }
+
     }
 
 }
